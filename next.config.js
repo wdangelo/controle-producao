d823095469a41a6ee5e6efe76c6418029e0d2aaf
William de Angelo <wdangelo1983@gmail.com>
2026-01-13 13:19:39 -0300

    Fix: Remover output standalone do next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb'
